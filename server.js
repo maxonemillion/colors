52b1124135648c0739bfae7535159f456f8ce7c1
maxonemillion <max.daniel@utah.edu>
2020-10-24 10:30:00 -0600

    small change

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const PORT = 7777
 app.use(express.static('./public'))
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join((__dirname, './public'));
+    res.sendFile(path.join((__dirname, './public')));
 });
 
 app.listen(PORT, function() {
     console.log("PORT: " + PORT);
-})
+});
